8f2f67162fe2786f0e3462db2e6c66dd23e811fe
Ramon Almeida <ramonjose.almeida@ufpe.br>
2025-05-15 02:33:50 -0300

    tela operacional

---
+++ pagina-de-acesso/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+function Home() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Operacional</h1>
+      <p>Você foi redirecionado após o login com sucesso.</p>
+    </div>
+  );
+}
+
+export default Home;
